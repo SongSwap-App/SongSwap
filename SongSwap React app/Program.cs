08a2a643fe3ac5f70be8618d524a0d7a6c22c89f
kvand <kvand@ORDI>
2023-12-12 19:03:27 +0100

    Add HttpClientFactory to startup

--- SongSwap React app/Program.cs
+++ SongSwap React app/Program.cs
@@ -1,9 +1,11 @@
 using SongSwap_React_app.Models.Services;
+using SongSwap_React_app.Startup;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<AuthenticationService>();
+builder.Services.AddCofiguredHttpClient();
 builder.Services.AddControllersWithViews();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ SongSwap React app/Startup/HttpClientConfiguration.cs
@@ -0,0 +1,19 @@
+﻿namespace SongSwap_React_app.Startup
+{
+    public static class HttpClientConfiguration
+    {
+        public static IServiceCollection AddCofiguredHttpClient(this IServiceCollection services)
+        {
+            services.AddHttpClient("SongSwapClient", client =>
+            {
+                client.BaseAddress = new Uri("https://api.musicapi.com/api/");
+                client.Timeout = TimeSpan.FromSeconds(30);
+
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+            });
+
+
+            return services;
+        }
+    }
+}
