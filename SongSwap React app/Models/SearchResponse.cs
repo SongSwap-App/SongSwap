cbe075bc57e9d24b9fbaa56efcf30bf537314e4e
kvand <kvand@ORDI>
2024-03-29 13:35:07 +0100

    Added SearchResponse model

---
+++ SongSwap React app/Models/SearchResponse.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SongSwap_React_app.Models
+{
+    [Serializable]
+    public class SearchResponse
+    {
+        [JsonPropertyName("results")]
+        public SearchItem[] Items { get; set; } = Array.Empty<SearchItem>();
+
+        [JsonPropertyName("nextParam")]
+        public string? NextParam { get; set; }
+
+        [JsonPropertyName("totalItems")]
+        public int? TotalItems { get; set; }
+    }
+
+    [Serializable]
+    public class SearchItem
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+    }
+}
